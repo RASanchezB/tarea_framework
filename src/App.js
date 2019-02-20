dcbfc6b686859be14afb6865c1f2e443aaf45890
RASanchezB <rasanchez@unitec.edu>
2019-02-19 18:32:04 -0600

    Hay algun error en el index

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
 class App extends Component {
   render() {
     return (
